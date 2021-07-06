5a0ef1460c76c5489d44aa5dcbabd59f2267845a
Lenar Batdalov <developer.lenar@gmail.com>
2021-07-06 07:41:34 +0300

    data binding and validation

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@ go mod init github.com/lenarbatdalov/learn
 
 tag v1 - добавил структуру, сервис и контроллер
 tag v2 - добавил logger, basic-auth и провел dump(gindump) + тестовый запрос(tester.go)
-
+tag v3 - data binding and validation
--- server.go
+++ server.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lenarbatdalov/learn/controller"
 	"github.com/lenarbatdalov/learn/middlewares"
 	"github.com/lenarbatdalov/learn/service"
-	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -26,7 +26,7 @@ func main() {
 
 	server := gin.New()
 
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 
 	server.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
@@ -39,8 +39,17 @@ func main() {
 	})
 
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Video Input is Valid!",
+			})
+		}
 	})
 
-	server.Run(":8080")
+	server.Run(":8081")
 }
